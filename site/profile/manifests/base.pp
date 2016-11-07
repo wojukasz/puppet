9e83f5e368e2c830e4d3047d1dc2b8aafb688a72
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-11-07 08:59:06 +0000

    Remove vim from package lists as conflicts with gvim on some systems.

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -21,8 +21,7 @@ class profile::base {
     key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD1fmreGnjXSxAEQ2YU7YuaHHGvgDdjVWRVZiXHNnQShHjOrCkPHEiMJcxoaUrFsqMT7IRjtQFPO6jy7ciKidpHMNIgDiZWa1mIHbCQ4biMV6dQMMT36oPIyiunlOn+cblH5nBC2SzJYgCixAJ+HIC5rMrikhUxfeSD6yfQ2GKmlLviNYkVphdUzl4IOQgmQRrRe/nQ9FANppbDVF1KbcpH6UFg6N65+tADAGvtkrqTsbD+jB712rEKxB7IhOmoQDdkO84lx/4HATv0JP7qOLWMErzX6E9pY6a1TCaaxzpgJ+V6McLVkdNnQ3D7erX2ymGwjlmYJ5s7p0UdVun3PDCB'
   }
 
-  package {['vim',
-           'sudo',
+  package {['sudo',
            'tmux',
            'htop',
            'rsync'
