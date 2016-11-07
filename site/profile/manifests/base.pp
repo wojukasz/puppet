3788466f6133e31341216bf88e275b8df966be7d
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-11-07 08:37:43 +0000

    Add user management to common profile.

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,3 +1,22 @@
 class profile::base {
   class {'::ntp': }
+
+  package { 'fish':
+    ensure => 'present'
+  }
+
+  user { 'alan':
+    ensure         => present,
+    gid            => 'alan',
+    groups         => [ 'sudo' ],
+    managehome     => true,
+    purge_ssh_keys => true,
+  }
+
+  ssh_authorized_key { 'alan':
+    ensure => present,
+    user   => 'alan',
+    type   => 'ssh-rsa',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD1fmreGnjXSxAEQ2YU7YuaHHGvgDdjVWRVZiXHNnQShHjOrCkPHEiMJcxoaUrFsqMT7IRjtQFPO6jy7ciKidpHMNIgDiZWa1mIHbCQ4biMV6dQMMT36oPIyiunlOn+cblH5nBC2SzJYgCixAJ+HIC5rMrikhUxfeSD6yfQ2GKmlLviNYkVphdUzl4IOQgmQRrRe/nQ9FANppbDVF1KbcpH6UFg6N65+tADAGvtkrqTsbD+jB712rEKxB7IhOmoQDdkO84lx/4HATv0JP7qOLWMErzX6E9pY6a1TCaaxzpgJ+V6McLVkdNnQ3D7erX2ymGwjlmYJ5s7p0UdVun3PDCB'
+  }
 }
