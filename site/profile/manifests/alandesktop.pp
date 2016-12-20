a6c3b4be0d08a771a4b1641fce5b75049e751d52
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-12-20 20:09:49 +0000

    Initial alandesktop profile.

---
+++ site/profile/manifests/alandesktop.pp
@@ -0,0 +1,26 @@
+class base::alandesktop {
+    $desktop_packages = [
+        'google-chrome',
+        'xorg-server',
+        'i3-vm',
+        'lightdm',
+        'lightdm-gtk-greeter',
+        'i3status',
+        'dmenu',
+        'termite',
+        'firefox',
+        'virtualbox',
+        'compton',
+        'feh',
+        'evince',
+        'libreoffice',
+        'inkscape',
+        'gimp',
+        'i3lock',
+        'shutter',
+        'gnome-web-photo',
+        'wireshark',
+        'xdot'
+    ]
+    package { $desktop_packages: ensure => latest }
+}
