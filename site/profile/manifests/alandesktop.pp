746f6479bdfa49cab97ae710401efb987faa7049
Alan Jenkins <alan.james.jenkins@gmail.com>
2016-12-25 06:33:59 +0000

    Correct wireshark and libreoffice package names.

--- site/profile/manifests/alandesktop.pp
+++ site/profile/manifests/alandesktop.pp
@@ -1,4 +1,4 @@
 class profile::alandesktop {
-    $desktop_packages = [ 'aria2', 'bind-tools', 'chromium', 'compton', 'dmenu', 'docker', 'docker-compose', 'dos2unix', 'evince', 'f2fs-tools', 'feh', 'firefox', 'gimp', 'git', 'gnome-web-photo', 'gnu-netcat', 'google-chrome', 'graphviz', 'gvim', 'i3-wm', 'i3lock', 'i3status', 'inkscape', 'iotop', 'libreoffice', 'lightdm', 'lightdm-gtk-greeter', 'nethogs', 'nmap', 'openssh', 'pkgfile', 'puppet', 'pwgen', 'shutter', 'termite', 'the_silver_searcher', 'tig', 'vagrant', 'virtualbox', 'wget', 'whois', 'wireshark', 'xdot', 'xorg-server' ]
+    $desktop_packages = [ 'aria2', 'bind-tools', 'chromium', 'compton', 'dmenu', 'docker', 'docker-compose', 'dos2unix', 'evince', 'f2fs-tools', 'feh', 'firefox', 'gimp', 'git', 'gnome-web-photo', 'gnu-netcat', 'google-chrome', 'graphviz', 'gvim', 'i3-wm', 'i3lock', 'i3status', 'inkscape', 'iotop', 'libreoffice-fresh-en-GB', 'lightdm', 'lightdm-gtk-greeter', 'nethogs', 'nmap', 'openssh', 'pkgfile', 'puppet', 'pwgen', 'shutter', 'termite', 'the_silver_searcher', 'tig', 'vagrant', 'virtualbox', 'wget', 'whois', 'wireshark-gtk', 'xdot', 'xorg-server' ]
     package { $desktop_packages: ensure => latest }
 }
