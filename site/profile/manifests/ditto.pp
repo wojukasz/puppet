5ad4bd58fc5604d5ca119ed547c14c8e79507bd5
Alan James Jenkins <alan.james.jenkins@gmail.com>
2017-01-01 19:56:23 +0000

    First attempt at couchpotato.

--- site/profile/manifests/ditto.pp
+++ site/profile/manifests/ditto.pp
@@ -98,16 +98,44 @@ class profile::ditto {
     mode    => '0775',
     require => File['/srv/deluge'],
   }
+  # {{{ couchpotato
+  file {'/usr/lib/systemd/system/couchpotato.service':
+    ensure  => file,
+    content => template('couchpotato/couchpotato.service.epp'),
+    notify  => Exec['systemctrl daemon-reload'],
+  }
+
+  user {'couchpotato':
+    ensure => present,
+    uid    => '1004',
+    gid    => '1004',
+  }
+
+  file {'/srv/couchpotato':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0775',
+  }
+
+  file {'/srv/couchpotato/config':
+    ensure  => directory,
+    owner   => 'couchpotato',
+    group   => 'couchpotato',
+    mode    => '0775',
+    require => File['/srv/couchpotato']
+  }
+
+  service {'couchpotato':
+    ensure  => running,
+    enable  => true,
+    require => [ User['couchpotato'], File['/usr/lib/systemd/system/couchpotato.service'], File['/srv/couchpotato/config'] ]
+  }
+  # }}}
 
   # file {'/usr/lib/systemd/system/sickrage.service':
   #   ensure  => file,
   #   content => template('sickrage/sickrage.service.epp'),
-  #   notify  => Exec['systemctrl daemon-reload'],
-  # }
-
-  # file {'/usr/lib/systemd/system/couchpotato.service':
-  #   ensure  => file,
-  #   content => template('couchpotato/couchpotato.service.epp'),
   #   notify  => Exec['systemctrl daemon-reload'],
   # }
 
