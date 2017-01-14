ef4f386379753fa0ce7283fc950f2a24c540b3f8
Alan James Jenkins <alan.james.jenkins@gmail.com>
2017-01-14 08:59:47 +0000

    Add vmwareguest profile.

---
+++ site/profile/manifests/vmwareguest.pp
@@ -0,0 +1,12 @@
+class profile::vmwareguest {
+    $vmware_packages = [
+      'open-vm-tools',
+      'xf86-input-vmmouse'
+    ]
+
+    service {'vmtoolsd':
+      enable  => true,
+      state => running,
+    }
+
+    package { $vmware_packages: ensure => latest }
