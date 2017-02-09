62a833f423b0165654cd359775abe7c3852f29a5
Alan James Jenkins <alan.james.jenkins@gmail.com>
2017-02-09 09:01:55 +0000

    Sort syntax and style problems in minecraft.pp.

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -13,28 +13,28 @@ class profile::jenkins {
     }
 
     file { '/home/jenkins':
-        ensure => directory,
-        owner => 'jenkins',
-        group => 'jenkins',
-        mode => '0770',
+        ensure  => directory,
+        owner   => 'jenkins',
+        group   => 'jenkins',
+        mode    => '0770',
         require => Package['jenkins'],
     } ->
     file { '/home/jenkins/.ssh':
         ensure => directory,
-        owner => 'jenkins',
-        group => 'jenkins',
-        mode => '0770',
+        owner  => 'jenkins',
+        group  => 'jenkins',
+        mode   => '0770',
     }
 
     file { '/etc/nginx/sites-available/jenkins.alan-jenkins.com':
-      ensure                                                                           => file,
-      owner                                                                            => 'root',
-      group                                                                            => 'root',
-      mode                                                                             => '0444',
-      content => epp('jenkins/jenkins.alan-jenkins.com.epp', { 'le_account_thumbprint' => hiera("le_account_thumbprint") })
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+      content => epp('jenkins/jenkins.alan-jenkins.com.epp', { 'le_account_thumbprint' => hiera('le_account_thumbprint') })
     }
 
-		file { '/etc/nginx/sites-enabled/jenkins.alan-jenkins.com':
+    file { '/etc/nginx/sites-enabled/jenkins.alan-jenkins.com':
       ensure => link,
       target => '/etc/nginx/sites-available/jenkins.alan-jenkins.com',
       notify => Service['nginx'],
