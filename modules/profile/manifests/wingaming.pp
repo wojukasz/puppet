bccde281a671f8e8c09bd118f676b903a5b9a663
Alan James Jenkins <alan.james.jenkins@gmail.com>
2017-03-12 10:09:30 +0000

    Add origin and uplay to be installed and clean up a bit.

--- modules/profile/manifests/wingaming.pp
+++ modules/profile/manifests/wingaming.pp
@@ -6,10 +6,36 @@ class profile::wingaming {
   package {'steam':
     ensure => latest,
   }
+
+  package {'uplay': 
+    ensure => latest,
+  }
+
+  package {'origin':
+    ensure => latest,
+  }
+
+  package {'battle.net':
+    ensure => latest,
+  }
+
   package {'retroarch':
     ensure => latest,
   }
+
   package {'gamesavemanager':
+    ensure => latest,
+  }
+
+  package {'mumble':
+    ensure => latest,
+  }
+
+  package {'discord':
+    ensure => latest,
+  }
+
+  package {'teamspeak':
     ensure => latest,
   }
 }
