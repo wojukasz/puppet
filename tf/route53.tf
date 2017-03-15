3e101b7acaf6a953e0412928cf89b66f9590edea
Alan James Jenkins <alan.james.jenkins@gmail.com>
2017-03-15 18:06:29 +0000

    Add records to hopefully setup SRV for skyfactory3 minecraft server.

---
+++ tf/route53.tf
@@ -0,0 +1,19 @@
+data "aws_route53_zone" "ajenkinscom" {
+    name = "alan-jenkins.com."
+}
+
+resource "aws_route53_record" "skyfactory3A" {
+    zone_id = "${data.aws_route53_zone.ajenkinscom.zone_id}"
+    name = "skyfactory3.alan-jenkins.com"
+    type = "A"
+    ttl = "60"
+    records = ["149.202.87.162"]
+}
+
+resource "aws_route53_record" "skyfactory3SRV" {
+    zone_id = "${data.aws_route53_zone.ajenkinscom.zone_id}"
+    name = "skyfactory3.alan-jenkins.com"
+    type = "SRV"
+    ttl = "60"
+    records = ["1 1 25566 _minecraft._tcp.skyfactory3.alan-jenkins.com"]
+}
