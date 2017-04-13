a0f330d84217e720bb0b89e1a27d3031d568da7f
Alan Jenkins <alan.james.jenkins@gmail.com>
2017-04-13 08:08:06 +0100

    Refactor code a bit.

--- modules/profile/manifests/libvirt_kvm.pp
+++ modules/profile/manifests/libvirt_kvm.pp
@@ -10,4 +10,21 @@ class profile::libvirt_kvm (
     service {'libvirtd':
         enable  => true,
     }
+
+    file_line {'qemu uefi config line':
+        ensure  => present,
+        path    => '/etc/libvirt/qemu.conf',
+        line    => 'nvram = ["/usr/share/ovmf/ovmf_code_x64.bin:/usr/share/ovmf/ovmf_vars_x64.bin"]',
+        match   => '^nvram\ ?\=\ ?\[[\s\w":/._,]*\]',
+        require => Package['libvirt'],
+        notify  => Service['libvirtd']
+    }
+
+    file {'/etc/polkit-1/rules.d/99-libvirt.rules':
+        ensure  => file,
+        content => epp('puppet:///data/libvirt/polkit_rules.epp'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+    }
 }
---
+++ modules/profile/manifests/monitor_hotplug.pp
@@ -0,0 +1,41 @@
+class profile::monitor_hotplug
+(
+  $user,
+  $int_display_name = 'eDP1',
+  $ext_display_name = 'DP1',
+  $ext_display_drm_path = '/sys/class/drm/card0-DP-1',
+  $locations = {},
+) {
+  exec {'systemctl daemon-reload':
+    command     => '/usr/bin/systemctl daemon-reload',
+    refreshonly => true,
+  }
+
+  # install monitor hotplug script
+  file {'/usr/local/bin/monitor_hotplug':
+    ensure  => file,
+    content => epp('data/monitor_hotplug/script.epp'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0744',
+  } ->
+
+  # install udev rule
+  file {'/etc/udev/rules.d/99-monitor_hotplug.rules':
+    ensure  => file,
+    content => epp('data/monitor_hotplug/udev_rules.epp'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+  } ->
+
+  # add systemd unit file
+  file {'/etc/systemd/system/monitor_hotplug.service':
+    ensure  => file,
+    content => epp('data/monitor_hotplug/systemd_service.epp'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    notify  => Exec['systemctl daemon-reload'],
+  }
+}
