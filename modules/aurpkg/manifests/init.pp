fa3c24139171e0d1346f630ac61944b8933eabb8
Alan Jenkins <alan.james.jenkins@gmail.com>
2017-04-24 07:30:20 +0100

    Move dmenu to i3 packages.

--- modules/aurpkg/manifests/init.pp
+++ modules/aurpkg/manifests/init.pp
@@ -17,19 +17,24 @@ define aurpkg (
     require => Exec['/usr/bin/tar xvf /tmp/cower.tar.gz']
   } ->
   exec { 'makepkg cower':
-    command => '/usr/bin/makepkg',
+    command => '/usr/bin/makepkg --skippgpcheck',
     cwd  => '/tmp/cower',
     user => $user,
     unless => '/usr/bin/pacman -Qi cower'
   } ->
-  exec { '/usr/bin/pacman -U cower.*.pkg.xz':
-    unless => '/usr/bin/pacman -Qi cower'
+  exec { '/usr/bin/pacman --noconfirm -U cower*.pkg.tar.xz':
+    cwd      => '/tmp/cower',
+    unless => '/usr/bin/pacman -Qi cower',
+    provider  => shell,
   } ->
   exec { '/usr/bin/rm -Rf /tmp/cower':
     onlyif => '/usr/bin/ls /tmp/cower'
   }
   # }}}
   # {{{ Install pacaur
+  package { 'expac':
+    ensure => latest,
+  } ->
   exec { '/usr/bin/curl https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz -o /tmp/pacaur.tar.gz':
     unless => '/usr/bin/pacman -Qi pacaur'
   } ->
@@ -50,14 +55,24 @@ define aurpkg (
     user    => $user,
     unless  => '/usr/bin/pacman -Qi pacaur'
   } ->
-  exec { '/usr/bin/pacman -U pacaur.*.pkg.xz':
-    unless => '/usr/bin/pacman -Qi pacaur'
+  exec { '/usr/bin/pacman --noconfirm -U pacaur*.pkg.tar.xz':
+    cwd      => '/tmp/pacaur',
+    unless   => '/usr/bin/pacman -Qi pacaur',
+    provider => shell,
   } ->
   exec { '/usr/bin/rm -Rf /tmp/pacaur':
     onlyif => '/usr/bin/ls /tmp/pacaur'
   }
   # }}}
   # {{{ if package not installed
+  exec { "/usr/bin/pacaur --noconfirm --noedit -S $title":
+    cwd         => "/home/$user",
+    provider    => shell,
+    environment => [
+      'HOME=/home/alan',
+    ],
+    user        => $user,
+  }
   # }}}
   # {{{ if package already installed
   ## Get current installed version
--- modules/profile/manifests/desktop.pp
+++ modules/profile/manifests/desktop.pp
@@ -6,9 +6,9 @@ class profile::desktop (
 {
     package { $desktop_packages: ensure => latest }
 
-    aur_key_trust { '487EACC08557AD082088DABA1EB2638FF56C0C53':
-        user => $packager
-    } ->
+    # aur_key_trust { '487EACC08557AD082088DABA1EB2638FF56C0C53':
+    #     user => $packager
+    # } ->
     aurpkg { $desktop_aur_packages:
         user => $packager
     }
